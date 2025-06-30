514bd2c34889e094e754c73ffa3159632bc3664b
Einon Moraes <you@exampeinommoraes@gmail.com>
2025-06-30 20:34:22 -0300

    feat(img): Adiciona  shadow e texto para background

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import './global.css'
 import styles from './App.module.css'
 import { Header } from './components/Header'
 import sacadaDeVidro from './assets/sacadaDeVidro.jpg'
-
 export function App() {
   
   return (
     <div>
       <Header/>
       <div className={styles.container}>
-        <img className={styles.backgroundImage} src='https://images.unsplash.com/photo-1494884113216-952a0c2c1a30?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        <img className={styles.backgroundImage} src={sacadaDeVidro}/>
+        <div className={styles.textOverlay}>
+          <strong >V&A Vidros e Esquadrias</strong>
+          <p>Soluções em vidros e esquadrias de alumínio para deixar sua casa mais bonita, segura e do seu jeito.</p>
+        </div>
       </div>
     </div>
   )
