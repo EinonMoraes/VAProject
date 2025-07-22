d15acc0f34bfb27ca568f41cbc3811b7e43e2a44
Einon Moraes <you@exampeinommoraes@gmail.com>
2025-07-21 21:18:56 -0300

    refactor: divide seção Home em componente separado

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import './global.css'
 import styles from './App.module.css'
 import { Header } from './components/Header'
-import sacadaDeVidro from './assets/sacadaDeVidro.jpg'
+import { Home } from './components/Home'
+
 export function App() {
   
   return (
     <div>
       <Header/>
-      <div className={styles.container}>
-        <img className={styles.backgroundImage} src={sacadaDeVidro}/>
-        <div className={styles.textOverlay}>
-          <strong >V&A Vidros e Esquadrias</strong>
-          <p>Soluções em vidros e esquadrias de alumínio para deixar sua casa mais bonita, segura e do seu jeito.</p>
-        </div>
-      </div>
+      <Home/>
     </div>
   )
 }
