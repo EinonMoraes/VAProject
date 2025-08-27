af1f9944451c5832e83328422733a8fef6537970
Einon Moraes <you@exampeinommoraes@gmail.com>
2025-08-26 23:21:49 -0300

    chore: adiciona componente card ao componente about

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,5 +1,9 @@
 import styles from './About.module.css'
 import sacada2 from '../../assets/imgs/sacada2.jpg'
+import { AboutCard, BenefitCard } from '../UI/Cards';
+import { LiaCoinsSolid } from "react-icons/lia"
+import { RiCheckDoubleFill } from 'react-icons/ri';
+import { FaHandshake } from 'react-icons/fa';
 
 export function About() {
     return (
@@ -10,6 +14,30 @@ export function About() {
         <div className={styles.backgroundContainer}>
             <div className={styles.backgroundImage}></div>
             <img src={sacada2} className={styles.sacada}/>
+            <div className={styles.card1}>    
+                <AboutCard    
+                    title="Nossa História"
+                    description= 'Fundada em 2023, a Sacada Bar e Restaurante nasceu do sonho de criar um espaço onde amigos e familiares pudessem se reunir para desfrutar de boa comida, música ao vivo e momentos inesquecíveis. Localizado no coração da cidade, nosso estabelecimento rapidamente se tornou um ponto de encontro popular, conhecido por sua atmosfera acolhedora e vibrante.'
+                />
+                <div className={styles.card2}>
+                    <BenefitCard
+                        IconComponent={LiaCoinsSolid}
+                        title="L0REM IPSUM"
+                        description='Lorem ipsum dolor sit ametLorem ipsum dolor sit amet'                      
+                    />
+                    <BenefitCard
+                        IconComponent={FaHandshake}
+                        title="L0REM IPSUM"
+                        description='Lorem ipsum dolor sit ametLorem ipsum dolor sit amet'
+                    />
+                    <BenefitCard
+                        IconComponent={RiCheckDoubleFill}
+                        title="L0REM IPSUM"
+                        description='Lorem ipsum dolor sit ametLorem ipsum dolor sit amet'
+                        
+                    />
+                </div>
+            </div> 
         </div>
         </>
     );
--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -7,7 +7,7 @@ export function Home () {
          <img className={styles.backgroundImage} src={heroImg}/>
          <div className={styles.textOverlay}>
             <strong >V&A Vidros e Esquadrias</strong>
-            <p>Soluções em vidros e esquadrias de alumínio para deixar sua casa mais bonita, segura e do seu jeito.</p>
+            <p>Soluções em vidros e esquadrias de alumínio para deixar sua casa mais bonita, segura e do seu jeito.</p>    
          </div>
       </div>
    )
